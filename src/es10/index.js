0e0a1bd41758222a5c422900b47c35f0a6f2cac8
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-29 16:36:43 -0600

    Update index.js

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,4 +1,4 @@
-let array = [1,2,3, [1,2,3, [1,2,3]]];
+let arrayy = [1,2,3, [1,2,3, [1,2,3]]];
 
 console.log(array.flat(2));
 
@@ -6,7 +6,7 @@ let array = [1,2,3,4,5];
 
 console.log(array.flatMap(value => [value, value * 2]));
 
-let hello = '        hello world';
+let hell = '        hello world';
 console.log(hello);
 console.log(hello.trimStart());
 
@@ -27,3 +27,19 @@ console.log(Object.fromEntries(entries));
 let mySymbl = `My Symbol`;
 let symbol = Symbol(mySymbl);
 console.log(symbol.description);
+
+//flatmap y trimstart trim end
+
+const arregloo = [1,2,3, [1,2,3 , [1,2,3]]];
+
+//matriz
+console.log(arregloo.flat(3))
+
+//mapea y aplana y transforma
+const arregloo2 = [1,2,3,4,5,6,7]
+console.log(arregloo2.flatMap(v => [v,v*2] ))
+
+//trimstart y trimend 
+var hola = "  Hola  MUndo  ";
+console.log(hola.trimStart())
+console.log(hola.trimEnd())
