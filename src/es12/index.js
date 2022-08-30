288b5314e0c0fef63d5387403a1605f5497ef2d9
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-29 18:16:58 -0600

    Update index.js

--- src/es12/index.js
+++ src/es12/index.js
@@ -1,3 +1,12 @@
 //numeric separators
 const value = 100_000_000;
 console.log(value);
+
+//replace all
+const text = "Hola mundo";
+const newText = text.replace(/\s/g, "-");
+console.log(newText);
+
+const palabra = "Isis es una persona muy buena";
+const newPalabra = palabra.replace("Isis","Isis Zapata");
+console.log(newPalabra);
